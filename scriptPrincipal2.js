d2fcca6f1d5537eac755de6705d36098e14089e2
MiguelRuiz <maicolangello@gmail.com>
2017-09-05 17:39:18 -0500

    en styksheet corrigio el cuadro  y en scriptPrincipal2 se agrego luz a la sombra

--- scriptPrincipal2.js
+++ scriptPrincipal2.js
@@ -8,9 +8,10 @@ function configurarApp ()
 }
 function dibujaFooter(canvas,contexto)
 {
+	contexto.fillStyle = "rgba(0,0,0,0.8)";
 	contexto.moveTo(0,0);
-	contexto.quadraticCurveTo(0,-50canvas.width-100,canvas.height-50);
+	contexto.quadraticCurveTo(0,-50,canvas.width-100,canvas.height-50);
 	contexto.fill();
-	//quadratucCurveTo(cpx,cpy,x,y)
+	//quadratucCurveTo(cpx,cpy,x,y);
 
 }
